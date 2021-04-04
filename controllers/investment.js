06d151a2bcfb05c2aa3fc31483b9fd897c0e3cd2
Anthony Hadwan <anthony.hadwan@hec.edu>
2021-04-04 13:51:01 +0200

    Added the get Edit route and controller actions for Investment

--- controllers/investment.js
+++ controllers/investment.js
@@ -50,3 +50,52 @@ exports.postAddInvestment = (req, res, next) => {
         })
 
 }
+
+
+exports.getEditInvestment = (req, res, next) => {
+    const invId = req.params.investmentId;
+    req.user
+      .getInvestments({where: { id: invId }, include: ['coin'] })
+      .then(investments => {
+        const investment = investments[0];
+        if (!investment) {
+          return res.redirect('/investments');
+        }
+        res.render('edit-investment', {
+          pageTitle: 'Edit Investment',
+          path: '/edit-investment',
+          investment: investment
+        });
+      })
+      .catch(err => console.log(err));
+  };
+  
+//   exports.postEditProduct = (req, res, next) => {
+//     const prodId = req.body.productId;
+//     const updatedTitle = req.body.title;
+//     const updatedPrice = req.body.price;
+//     const updatedImageUrl = req.body.imageUrl;
+//     const updatedDesc = req.body.description;
+//     Product.findByPk(prodId)
+//       .then(product => {
+//         product.title = updatedTitle;
+//         product.price = updatedPrice;
+//         product.description = updatedDesc;
+//         product.imageUrl = updatedImageUrl;
+//          return product.save()
+//       })
+//       .then(result => {
+//         console.log('UPDATED PRODUCT')
+//         res.redirect('/admin/products');
+//       } )
+//       .catch(err => console.log(err)) // thanks to the return above we can catch  errors for the both of the promises above
+    // const updatedProduct = new Product(
+    //   prodId,
+    //   updatedTitle,
+    //   updatedImageUrl,
+    //   updatedDesc,
+    //   updatedPrice
+    // );
+    // updatedProduct.save();
+    // res.redirect('/admin/products'); if we leave this here we wont see the updates directly due to the async JS 
+//   };
--- routes/investment.js
+++ routes/investment.js
@@ -6,10 +6,16 @@ const investmentController = require('../controllers/investment');
 
 const router = express.Router();
 
+
+// Displaying Investments Route
 router.get('/investments', investmentController.getInvestments);
+
+// Adding an Investment Routes
 router.get('/new-investment', investmentController.getAddInvestment);
-
-
 router.post('/new-investment', investmentController.postAddInvestment);
+
+
+//Editing an Investment Routes
+router.get('/edit-investment/:investmentId', investmentController.getEditInvestment);
 
 module.exports = router;
